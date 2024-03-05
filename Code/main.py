a7e2f3193c964359d48e586ed67645b6a28719e9
James Fogwill <james@robfogwill.com>
2024-03-05 17:33:39 +0000

    Code Mk1

---
+++ Code/main.py
@@ -0,0 +1,39 @@
+# load necessary libraries
+import pandas as pd
+import networkx as nx
+import problem
+
+# load san fransisco map data
+nodes_df = pd.read_csv("nodes_l.csv")
+edges_df = pd.read_csv("edges_l.csv")
+
+# print the data to test
+print(nodes_df)
+print(edges_df)
+
+networkMap = nx.Graph()
+
+for index, row in nodes_df.iterrows():
+    networkMap.add_node(
+        row['node_id'],
+        longitude=row['longitude'],
+        latitude=row['latitude'],
+        altitude=row['altitude']
+    )
+
+for index, row in edges_df.iterrows():
+    networkMap.add_edge(
+        # row['edge_id'],
+        row['source'],  # source node id
+        row['target'],  # target node id
+        length=row['length'],
+        car=row['car'],
+        car_reverse=row['car_reverse'],
+        bike=row['bike'],
+        bike_reverse=row['bike_reverse'],
+        foot=row['foot']
+    )
+
+prob = problem.shortestPathProblem(networkMap)
+
+prob.displayMap()
---
+++ Code/problem.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+class shortestPathProblem:
+
+    def __init__(self, problemMap):
+
+        # networkX graph representing the city, nodes are places, edges are roads
+        self.problemMap = problemMap
+
+    def displayMap(self):
+        print("Drawing Graph")
+        positions = nx.spring_layout(self.problemMap, iterations=10)
+        print("middle")
+        nx.draw(self.problemMap, positions, with_labels=True, node_size = 300)
+        print("Layout Calculated")
+        plt.show()
+
+    # evaulates the path/solution
+    def evaluate(self, path):
+        # set distance and time objectives
+        totalTime = 0
+        totalDist = 0
+
+        # iterate through nodes
+        for i in range(len(path - 1)):
+
+            startNode = path[i] # get the start & end nodes
+            endNode = path[i+1]
+            edgeData = self.problemMap.get_edge_data(startNode,endNode) # work out edge data between them
+
+            # get the distance between nodes and speed limit of edge
+            distance = edgeData.get("length",0)
+            speedLimit = edgeData.get("car",0)
+
+            # speed is in the form of 0-6
+
+            time = distance/speedLimit
+
+
